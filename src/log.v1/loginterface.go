ba9b53a7fc6d35f6385ab1aa4940404b2fa68c24
zhouzhentao <zhouzhentao@qiniu.com>
2020-09-27 13:52:32 +0800

    update

--- src/log.v1/loginterface.go
+++ src/log.v1/loginterface.go
@@ -1,12 +1,3 @@
-/*
-	改善能直接调用并且可以自由选择终端或者文件或者网络方式format输出日志
-	设计思路：
-		易用性封装
-		默认情况下执行直接输出到终端，如果读取到配置文件里面有logpath的情况下 输出到logpath，如果有kafka配置就输出到kakfa
-		输出文件名行号
-		文件输出不同 errorlog 输出errorlog文件 常规输出常规
-*/
-
 package log
 
 import (
